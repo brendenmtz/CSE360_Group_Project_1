666b490c2647b462dccf0fabb229cd9c2b8aed8d
Jannis Grimm <70415018+iamjannisgrimm@users.noreply.github.com>
2022-03-24 13:14:45 -0700

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,22 @@
+public class Address {
+    public String street;
+    private String town;
+    private int zip;
+    private String state;
+
+    //Constructor for Address
+    public Address(String street, String town, int zip, String state) {
+        this.street = street;
+        this.town = town;
+        this.zip = zip;
+        this.state = state;
+    }
+
+    public void addressToString() {
+        System.out.print("ADDRESS:");
+        System.out.println(this.street);
+        System.out.println(this.town);
+        System.out.println(this.zip);
+        System.out.println(this.state);
+    }
+}
---
+++ User.java
@@ -0,0 +1,5 @@
+public class User {
+    public String username;
+    public String password;
+
+}
